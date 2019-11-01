2e4cdd6da0360707d653026cca86df40efe0d060
Jordan McCollam <jordy.mccollam@gmail.com>
2019-11-01 17:50:46 -0500

    Fixed post request for new family

--- public/logic.js
+++ public/logic.js
@@ -1,18 +1,36 @@
+var loggedIn = false;
+
 $(document).ready(function() {
+    $("#welcomeMessage").hide();
 
     $("#startFamily").on("click", function() {
+        event.preventDefault();
         $("#newFamilyModal").modal("show");
     });
 
     $("#newFamilySubmit").on("click", function() {
-        $("#newFamilyModal").modal("hide");
+        event.preventDefault();
+        
         var newFamily = {
             name: $("#newFamilyName").val(),
             password: $("#newPassword").val()
         };
 
-        
-    })
+        $.post("/api/families", newFamily);
+        newLogin(newFamily.name);
+
+        $("#newFamilyModal").modal("hide");
+    });
+
+    function newLogin(name) {
+        $("#loginBtn").hide();
+        $("#welcomeMessage").show();
+        $("#welcomeMessage").html("Welcome, " + name);
+        loggedIn = true;
+    };
+
+
+
 
 // End of jQuery
 })
