25a4f54541506eb50c98f4da19073327f68f14e0
Jordan McCollam <jordy.mccollam@gmail.com>
2019-11-01 12:51:02 -0500

    Configured handlebars

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require("express");
+var handlebars = require("express-handlebars");
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Parse config
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Handlebars Config
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 // Listener
 app.llisten(PORT, function() {
