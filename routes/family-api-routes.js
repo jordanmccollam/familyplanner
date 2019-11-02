2cc76327cc516a0b38938f861c9175099ba96100
Jordan McCollam <jordy.mccollam@gmail.com>
2019-11-01 22:21:25 -0500

    Configured to send usernames and info

--- routes/family-api-routes.js
+++ routes/family-api-routes.js
@@ -1,10 +1,13 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
+    app.get("/api/families", function(req, res) {
+        db.Family.findAll().then(function(data) {
+            res.json(data);
+        })
+    });
 
     app.post("/api/families", function(req, res) {
-        // console.log(req.body);
         db.Family.create(req.body).then(function(data) {
             res.json(data);
         });
